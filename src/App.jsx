6121a835dca63912fbd1a6791045149ffecb9c11
DuckQuack <bunchhengchheav@gmail.com>
2024-03-30 23:30:33 +0700

    Vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import HomePage from './assets/components/homepage/Home_Page.jsx';
 import TopNavbar from './assets/components/navbar/Navbar.jsx';
-
+import SideNavbar from "./assets/components/navbar/Sidebar.jsx";
 function App() {
   return (
-    <div className='flex flex-col items-start'>
+    <div className='flex flex-col p-0 m-0'>
       <div>
         <HomePage></HomePage> 
       </div>
       <div>
         <TopNavbar></TopNavbar>
+      </div>
+      {/* this block gonna be the student list that include "Sidebar", "Student Database","StudentDetail" */}
+      <div>
+        <div className='w-20 bg-slate-600 mt-10'>
+          <SideNavbar></SideNavbar>
+        </div>
       </div>
     </div>
   );
---
+++ src/assets/components/navbar/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <div className="w-20 h-screen flex flex-col">
+      <a href="#">Duck</a>
+      <a href="#">Duck</a>
+      <a href="#">Duck</a>
+      <a href="#">Duck</a>
+      <a href="#">Duck</a>
+    
+    </div>
+  );
+}
+
+export default Sidebar;
