450ea30c091f4c7d5649da6496d131fb049aa08d
DuckQuack <bunchhengchheav@gmail.com>
2024-04-01 16:15:19 +0700

    Vite

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,28 @@ import React from 'react';
 import HomePage from './assets/components/homepage/Home_Page.jsx';
 import TopNavbar from './assets/components/navbar/Navbar.jsx';
 import SideNavbar from "./assets/components/navbar/Sidebar.jsx";
+import Signup from './assets/components/signup/Signup.jsx';
+
 function App() {
   return (
-    <div className='flex flex-col p-0 m-0'>
-      <div>
-        <HomePage></HomePage> 
-      </div>
-      <div>
-        <TopNavbar></TopNavbar>
-      </div>
-      {/* this block gonna be the student list that include "Sidebar", "Student Database","StudentDetail" */}
-      <div>
-        <div className='w-20 bg-slate-600 mt-10'>
-          <SideNavbar></SideNavbar>
-        </div>
-      </div>
+    // Signup Page
+    <div className=''>
+      <Signup></Signup>
     </div>
+    // <div className='flex flex-col p-0 m-0'>
+    //   <div>
+    //     <HomePage></HomePage> 
+    //   </div>
+    //   <div>
+    //     <TopNavbar></TopNavbar>
+    //   </div>
+    //   {/* this block gonna be the student list that include "Sidebar", "Student Database","StudentDetail" */}
+    //   <div>
+    //     <div className='w-20 bg-slate-600 mt-10'>
+    //       <SideNavbar></SideNavbar>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
 
--- src/assets/components/signup/Signup.jsx
+++ src/assets/components/signup/Signup.jsx
@@ -1,9 +1,20 @@
 import React from "react";
-
+import "./Signup.css";
 function Signup() {
     return (
-        <div>
+        <div className="grid place-content-center align-middle w-screen h-screen">
             <h1>Signup</h1>
+
+            <div className="UserName flex">
+            {/* User Name */}
+                <div>
+                    <input type="text" placeholder="First Name"/>
+                    <input type="text" placeholder="Last Name"/>
+                </div>
+                <input type="text" placeholder="Email" className="Duck bg-slate-700" p-8/>
+
+            </div>
         </div>
     );
 }
+export default Signup;
