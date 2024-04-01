6093a9aa967313c4a6410d2fce0171b023e00208
DuckQuack <bunchhengchheav@gmail.com>
2024-04-01 15:23:04 +0700

    Teaching

--- src/assets/components/navbar/Sidebar.jsx
+++ src/assets/components/navbar/Sidebar.jsx
@@ -2,13 +2,12 @@ import React from "react";
 
 function Sidebar() {
   return (
-    <div className="w-20 h-screen flex flex-col">
-      <a href="#">Duck</a>
-      <a href="#">Duck</a>
-      <a href="#">Duck</a>
-      <a href="#">Duck</a>
-      <a href="#">Duck</a>
-    
+    <div className="w-20 h-screen flex flex-col justify-around align-middle">
+      <a href="#" className="">Duck</a>
+      <a href="#" className="">Duck</a>
+      <a href="#" className="">Duck</a>
+      <a href="#" className="">Duck</a>
+      <a href="#" className="">Duck</a>
     </div>
   );
 }
